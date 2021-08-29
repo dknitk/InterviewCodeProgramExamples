7a5c2887b09dfd5209c3e01260088fba596b55d9
Dharmendra Sahu <dknitk@rediffmail.com>
2021-08-29 14:27:43 -0400

    Java interview program code

---
+++ src/com/kw/interview/programs/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.kw.interview.programs;
+
+/**
+ * This sample class helps to print hello world program.
+ */
+public class HelloWorld {
+
+    /**
+     * This method helps to print hello world program.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, Welcome to Hello World Application!");
+    }
+}
